fad4e43a3184523f7a7329f8079a5d8a86475113
LinaOskol <124796450+LinaOskol@users.noreply.github.com>
2024-03-28 22:19:35 +0600

    feat: create one tab

---
+++ src/components/tabs/twotabs.js
@@ -0,0 +1,13 @@
+import React from "react";
+import List from "../list/list";
+import "./tab.css";
+const Two = () => {
+  return (
+    <div className="content-tab-two">
+      <div className="List">
+        <List></List>
+      </div>
+    </div>
+  );
+};
+export default Two;
